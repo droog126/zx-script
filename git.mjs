6eb2a5a396220b9aea1e9f65b6815871ac947f11
droog126 <59406205+droog126@users.noreply.github.com>
2021-08-31 18:06:39 +0800

    testgi

--- git.mjs
+++ git.mjs
@@ -3,6 +3,10 @@ var url='https://github.com/droog126/zx-script.git'
 await $`git init`
 await $`git add *`
 await $`git commit -m "testgi"`
-await $`git remote rm ${name}`
+try {
+    await $`git remote rm ${name}`
+} catch (err) {
+    console.log('没有这个远程仓库')
+}
 await $`git remote add ${name} ${url}`
 await $`git push ${name} master`
